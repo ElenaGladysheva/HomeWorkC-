d2a65348bc1b72d465842c7feb0605ee96108a7e
Elena Gladysheva <gladen000@gmail.com>
2022-10-20 12:37:54 +0300

    Task 13 solved.

--- HomeWork_1/Homework_2/Program.cs
+++ HomeWork_1/Homework_2/Program.cs
@@ -1,6 +1,6 @@
 ﻿//ДЗ 2.
 // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
+/*
 int TwoNumber (int num)
 {
     int result;
@@ -12,15 +12,33 @@ Console.Clear();
 Console.Write("Input three-digital number: ");
 int inputNum = Convert.ToInt32(Console.ReadLine());
 int result = TwoNumber(inputNum);
-Console.WriteLine($"The second digital of number {inputNum} is {result}.");
+Console.WriteLine($"The second figure of number {inputNum} is {result}.");
+*/
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-// 645 -> 5
+void ThirdFigureCut (int num)
+{   
+    int inpNum = num;
+    int result = 0;
+    if (num < 100) 
+        Console.WriteLine("No third figure!");
+    else
+    {
+        while (num >= 1000)
+            {
+                num = num / 10;   
+            }
+        int number = num % 10;
+        Console.WriteLine($"The third figure of number {inpNum} is {number}.");
+    }   
+    
+}
+Console.Clear();
+Console.Write("Input integer number: ");
+int inputNumber = Convert.ToInt32(Console.ReadLine());
 
-// 78 -> третьей цифры нет
-
-// 32679 -> 6
+ThirdFigureCut(inputNumber);
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
