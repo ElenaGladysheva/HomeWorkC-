dba46cdc9471bf693d8ff0d0c420ebb4b02de3c4
Elena Gladysheva <gladen000@gmail.com>
2022-10-24 15:18:24 +0300

    Reading to array in task 19.

--- HomeWork_3/Program.cs
+++ HomeWork_3/Program.cs
@@ -9,6 +9,60 @@
 // 12821 -> да
 
 // 23432 -> да
+bool Polindrom5Number (int[] array)
+{   
+    int n = array.Length;
+    int index = 0;
+    bool result = true;
+    while (index < n/2)
+    {
+        if (array[index] != array[n-index-1])
+        {
+            result = false;
+            index = n-1;
+        }
+        else
+        {   
+            index++; 
+        }
+        
+    }
+    return result;
+}
+void FillArray(int number)
+{
+    double[] array = {0,0,0,0,0};
+    int lenght = array.Length;
+    int index = 0;
+    while (index <= lenght-1)
+    {
+        double power10 = Math.Round(Math.Pow(10,lenght-index-1),0);
+        if (index != 0)
+        {
+            array[index] = Math.Round((number / power10),0) % 10;
+            Console.Write(array[index] +" ");
+            index++;
+        }
+        else
+        {
+            array[index] = Math.Round((number / power10),0);
+            Console.Write(array[index] +" ");
+            index++; 
+        }
+    }
+
+}
+Console.Clear();
+Console.Write("Input five-digital number: ");
+int inputNum = Convert.ToInt32(Console.ReadLine());
+
+FillArray(inputNum);
+
+// if (Polindrom5Number(array)
+//      Console.WriteLine("Yes");
+// else
+//     Console.WriteLine("No"); 
+
 
 
 // Задача 21
@@ -18,7 +72,7 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
-
+/*
 double Distance3D(double xA, double yA, double zA, double xB, double yB, double zB)
 {
     double result;
@@ -42,7 +96,7 @@ double inputZB = Convert.ToDouble(Console.ReadLine());
 
 double result = Distance3D(inputXA, inputYA, inputZA, inputXB, inputYB, inputZB);
 Console.WriteLine(result);
-
+*/
 // Задача 23
 
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
