9adcfe1bf93491cc049f1425a4bb07f1258e0b42
Elena Gladysheva <gladen000@gmail.com>
2022-10-24 11:13:14 +0300

    Task 21 solved.

--- HomeWork_3/Program.cs
+++ HomeWork_3/Program.cs
@@ -19,13 +19,37 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+double Distance3D(double xA, double yA, double zA, double xB, double yB, double zB)
+{
+    double result;
+    result = Math.Sqrt(Math.Pow((xB-xA),2)+Math.Pow((yB-yA),2)+Math.Pow((zB-zA),2));
+    result = Math.Round(result,2);
+    return result;
+}
+Console.Clear();
+Console.Write("Input coordinate xA: ");
+double inputXA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input coordinate yA: ");
+double inputYA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input coordinate zA: ");
+double inputZA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input coordinate xB: ");
+double inputXB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input coordinate yB: ");
+double inputYB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input coordinate zB: ");
+double inputZB = Convert.ToDouble(Console.ReadLine());
+
+double result = Distance3D(inputXA, inputYA, inputZA, inputXB, inputYB, inputZB);
+Console.WriteLine(result);
+
 // Задача 23
 
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
-
+/*
 void DegreeCube(int num)
 {
     int current = 1;
@@ -44,5 +68,5 @@ if (inputNum <= 0)
    Console.WriteLine("Wrong number!");
 else
    DegreeCube(inputNum);
-
+*/
 
