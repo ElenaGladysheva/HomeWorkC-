47312cb5e34bdd947272e94b80212052c4e9fb8d
Elena Gladysheva <gladen000@gmail.com>
2022-11-12 11:29:08 +0300

    Task 27 solved.

--- HomeWork_4/Program.cs
+++ HomeWork_4/Program.cs
@@ -1,6 +1,6 @@
 ﻿//ДЗ 4.
 // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-
+/*
 int DegreeAB (int a, int b)
 {
     int result = 1;
@@ -16,7 +16,7 @@ Console.Write("Input B: ");
 int InpB = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"The number {inpA} to the power of {InpB} is {DegreeAB(inpA, InpB)}");
-
+*/
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
 // 452 -> 11
@@ -24,6 +24,28 @@ Console.WriteLine($"The number {inpA} to the power of {InpB} is {DegreeAB(inpA, 
 // 82 -> 10
 
 // 9012 -> 12
+
+int SumDigitInNumber (int number)
+{
+    int result = 0;
+    int currentNum = number;
+    int power = 1;
+
+    while(currentNum !=0)
+    {
+        currentNum = currentNum / 10;
+        result = result + number % (10*power)/power;
+        power *= 10;
+
+    }
+    return result;
+
+}
+
+Console.Clear();
+Console.Write("Input number: ");
+int inpN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The sum of digit in number {inpN} is {SumDigitInNumber(inpN)}.");
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
