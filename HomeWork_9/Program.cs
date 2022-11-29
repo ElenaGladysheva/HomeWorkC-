e114bfbd74234516a485bfeece602beb1b6bcc49
Elena Gladysheva <gladen000@gmail.com>
2022-11-29 15:53:47 +0300

    Task 68 solved.

--- HomeWork_9/Program.cs
+++ HomeWork_9/Program.cs
@@ -20,6 +20,7 @@ ShowNumbers(n);
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+/*
  int SumMtoN (int m, int n)
  {
     if (m <= n) return SumMtoN (m+1, n) + m;
@@ -31,7 +32,29 @@ int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input N: ");
 int n = Convert.ToInt32(Console.ReadLine()); 
 Console.WriteLine($"The sum of number is {SumMtoN (m, n)}."); 
+*/
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+int FunctionAkkerman(int m, int n)
+{
+    if (m == 0) return  n+1;
+    else 
+        if ((m != 0) && (n == 0)) return FunctionAkkerman(m - 1, 1);
+        else
+            return FunctionAkkerman(m - 1, FunctionAkkerman(m, n - 1));
+
+
+}
+
+Console.Clear();
+Console.Write("Input M: ");
+int m = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(m < 0 || n < 0) Console.WriteLine("Invalid entered values!");
+else
+    Console.WriteLine(FunctionAkkerman(m, n));
